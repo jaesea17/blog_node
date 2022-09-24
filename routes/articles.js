3f868fbfe841a23428c2f98ff84d154b1375d178
Oforkaja Joseph <oforkajajoseph@gmail.com>
2022-09-24 19:59:34 +0100

    change Welcome to be editable

--- routes/articles.js
+++ routes/articles.js
@@ -14,7 +14,7 @@ router.get('/edit/:id', async (req, res) => {
 router.get('/:slug', async (req, res) => {
     const article = await Article.findOne({ slug: req.params.slug });
     if (article == null) res.redirect('/');
-    res.render('articlesfolder/show', { article: article});
+    res.render('articlesfolder/show', { article: article });
 })
 
 router.post('/', async (req, res, next) => {
@@ -27,23 +27,23 @@ router.post('/:slug', async (req, res) => {
     if (req.params.slug === "welcome") {
         console.log("developer's article immutable")
         res.render('articlesfolder/editanddelete')
-    }else{
+    } else {
         await Article.findOneAndDelete({ slug: req.params.slug })
         res.redirect('/');
     }
-    
+
 })
 
 //actually "put" not best practice but only way out i could find
-router.post('/update/:slug', async (req, res, next) => {
-    if (req.params.slug === "welcome") {
-        console.log("developer's article immutable")
-        res.render('articlesfolder/editanddelete')
-    }else{
-        req.article = await Article.findOne({ slug: req.params.slug });
-        next();
-    }
-}, saveArticleAndRedirect('edit'))
+// router.post('/update/:slug', async (req, res, next) => {
+//     if (req.params.slug === "welcome") {
+//         console.log("developer's article immutable")
+//         res.render('articlesfolder/editanddelete')
+//     }else{
+//         req.article = await Article.findOne({ slug: req.params.slug });
+//         next();
+//     }
+// }, saveArticleAndRedirect('edit'))
 
 
 
@@ -53,13 +53,13 @@ function saveArticleAndRedirect(path) {
         article.title = req.body.title;
         article.description = req.body.description;
         article.markdown = req.body.markdown;
-        try{
+        try {
             let savedArticle = await article.save()
             res.redirect(`/articles/${savedArticle.slug}`)
-         }catch (err) {
-             console.log(err);
-             res.render(`articlesfolder/${path}`, { article: article });
-         }
+        } catch (err) {
+            console.log(err);
+            res.render(`articlesfolder/${path}`, { article: article });
+        }
     }
 }
 
